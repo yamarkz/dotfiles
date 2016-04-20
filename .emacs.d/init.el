48af8e791a3633e6867f32e5a9f033b456bd7717
ykazuki <y.kazuki0614n@gmail.com>
2016-04-21 01:02:38 +0900

    init.el fix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,4 @@
-				      ;-*- Mode:Emacs-Lisp;Coding: utf-8 -*-
+              ;-*- Mode:Emacs-Lisp;Coding: utf-8 -*-
 ;; --------------------------------------------------
 ;; @load-path
 
@@ -7,9 +7,9 @@
   (let (path)
     (dolist (path paths paths)
       (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
-	(add-to-list 'load-path default-directory)
-	(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-	    (normal-top-level-add-subdirs-to-load-path))))))
+  (add-to-list 'load-path default-directory)
+  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+      (normal-top-level-add-subdirs-to-load-path))))))
 
 ;; load-pathに追加するフォルダ
 ;; 2つ以上フォルダを指定する場合の引数 => (add-to-path "elisp" "xxx" "xxx")
@@ -25,31 +25,31 @@
 (set-language-environment "Japanese")
 (let ((ws window-system))
   (cond ((eq ws 'w32)
-	 (prefer-coding-systems 'utf-8-unix)
-	 (set-default-coding-systems 'utf-8-unix)
-	 (setq file-name-coding-system 'sjis)
-	 (setq locale-coding-system 'utf-8))
-	((eq ws 'ns)
-	 (require 'ucs-normalize)
-	 (prefer-coding-system 'utf-8-hfs)
-	 (setq file-name-coding-system 'utf-8-hfs)
-	 (setq locale-coding-system 'utf-8-hfs))))
+   (prefer-coding-systems 'utf-8-unix)
+   (set-default-coding-systems 'utf-8-unix)
+   (setq file-name-coding-system 'sjis)
+   (setq locale-coding-system 'utf-8))
+  ((eq ws 'ns)
+   (require 'ucs-normalize)
+   (prefer-coding-system 'utf-8-hfs)
+   (setq file-name-coding-system 'utf-8-hfs)
+   (setq locale-coding-system 'utf-8-hfs))))
 
 ;; Macで英数と日本語にRictyを指定
 (let ((ws window-system))
   (cond ((eq ws 'w32)
      (set-face-attribute default nil
                 :family "Meiryo" ;; 変数
-		:height 100)
+    :height 100)
      (set-fontset-font nil japanese-jisx0208 (font-spec :family "Meiriyo"))) ;; 日本語
     ((eq ws 'ns)
      (set-face-attribute default nil
-		 :family "Ricty" ;; 英数
-		 :height 140)
+     :family "Ricty" ;; 英数
+     :height 140)
      (set-fontest-font nil japanese-jisx0208 (font-spec :family "Ricty"))))) ;; 日本語
 
 ;; スタートアップ非表示
-(setq inhibit-startup-screeen t)
+(setq inhibit-startup-screen t)
 
 ;; scratchの初期メッセージ消去
 (setq initial-scratch-message "")
@@ -70,8 +70,8 @@
 ;; 行番号表示
 (global-linum-mode t)
 (set-face-attribute 'linum nil
-	    :foreground "#800"
-	    :height 0.9)
+      :foreground "#800"
+      :height 0.9)
 
 ;; 行番号フォーマット
 ;; (setq linum-format "%4d")
@@ -86,6 +86,9 @@
 
 ;; 選択領域の色
 (set-face-background 'region "#555")
+
+
+
 
 ;; 行末の白色を強調表示
 (setq-default show-trailing-whitespace t)
@@ -148,83 +151,6 @@
 (defvar my-lines-page-mode t)
 (defvar my-mode-line-format)
 
-(when my-lines-page-mode
-  (setq my-mode-line-format "%d")
-  (if size-indication-mode
-      (setq my-mode-line-format (concat my-mode-line-format "of %%|")))
-  (cond ((and(eq line-number-mode t) (eq column-number-mode t))
-         (setq my-mode-line-format (concat my-mode-line-format "(%%|,%%c)")))
-        ((eq line-number-mode t)
-         (setq my-mode-line-format (concat my-mode-line-format "L%%|")))
-        ((eq column-number-mode t)
-         (setq my-mode-line-format (concat my-mode-line-format "C%%x"))))
-
-  (setq mode-line-position
-        '(:eval (format my-mode-line-format
-                        (count-lines (point-max)(point-min))))))
-
-;; -----------------------------------------------------------
-;; @initial frame maximize
-
-;; 起動時にウィンドウ最大化
-;; http://www.emacswiki.org/emacs/FullScreen#toc12
-(defun jbr-init()
- "Called from term-setup-hook after the default
-  terminal setup is
-  done or directly from startup if term-setup-hook not
-  used. The value
-  0xF030 is the command for maximizing a window."
- (interactive)
- (w32-send-sys-command #xf030)
- (ecb-redraw-layout)
- (calendar))
-
-(let ((ws window-system))
-  (cond ((eq ws 'w32)
-         (set-trame-position (selected-frame) 0 0)
-         (setq term-setup-hook 'jbr-init)
-         (setq window-setup-hook 'jbr-init))
-        ((eq ws 'ns)
-         ;; for MacBookAir
-         (set-frame-position (selected-frame) 0 0)
-         (set-frame-size (selected-frame) 95 47 ))))
-
-;; -----------------------------------------------------------
-;; @ key bind
-
-;; バックスラッシュ
-(define-key global-map (kbd "M-|") "\\")
-
-;; globalなC-zを無効化
-(global-unset-key "\C-z")
-
-
-;; ----------------------------------------------------------
-;; @ auto-install.el
-
-;; パッケージのインストールを自動化
-;; http://www.emacswiki.org/emacs/auto-install.el
-(when (require 'auto-install nil t)
-  (setq auto-install-directory "~/.emacs.d/elisp/")
-  (auto-install-update-emacswiki-package-name t)
-  (auto-install-compatibility-setup))
-
-;; ----------------------------------------------------------
-;; @ color-theme.el
-
-;; Emacsのカラーテーマ
-;; http://code.google.com/p/gnuemacscolorthemetest/
-(when (and (require 'color-theme nil t)(window-system))
-  (color-theme-initialize)
-  (color-theme-clarity)
-
-;; ---------------------------------------------------------
-;; @ redo+.el
-
-;; redoできるようにする
-;; http://www.emacswiki.org/emacs/redo+.el
-(when (require 'redo+ nil t)
-  (define-key global-map (kbd "C-_") 'redo))
 
 ;; ---------------------------------------------------------
 ;; @menu-tree.el
@@ -232,8 +158,8 @@
 ;; メニューバー日本語化
 ;; http://www11.atwiki.jp/s-irie/pages/13.html
 (if (and(= emacs-major-version 22)
-        (eq window-system x))
-    (setq menu-tree-coding-system utf-8))
+        (eq window-system 'x))
+    (setq menu-tree-coding-system 'utf-8))
 (require 'menu-tree nil t)
 
 
@@ -253,12 +179,10 @@
         (remove-if
          (lambda(buffer)
            (unless (string-match (buffer-name buffer)
-                         "\\(*scraych*\\|*Apropos*\\|*shell*\\|*eshell*\\|*Customize*\\)"
+                         "\\(*scratch*\\|*Apropos*\\|*shell*\\|*eshell*\\|*Customize*\\)")
             (find (aref (buffer-name buffer) 0) "*"))
            )
            (buffer-list))))
-;; tabbarを有効にする
-(tabbar-mode 1)
 
 ;; ボタンをシンプルにする
 (setq tabbar-home-button-enabled "")
@@ -266,3 +190,74 @@
 (setq tabbar-scroll-left-button-enabled "")
 (setq tabbar-scroll-right-button-disabled "")
 (setq tabbar-scroll-left-button-disabled "")
+
+;; Ctrl-Tab, Ctrl-Shift-Tab でタブを切り替える
+(dolist (func '(tabbar-mode tabbar-forward-tab tabbar-forward-grou@ tabbar-backward-tab tabbar-backward-group))
+  (autoload func "tabbar" "Tabs at the top of buffers and easy control-tab navigation"))
+(defmacro defun-prefix-alt (name on-no-prefix on-prefix &optional do-always)
+  '(defun ,name (arg)
+     (interactive "P")
+     ,do-always
+     (if (equal nil arg)
+         ,on-no-prefix
+       ,on-prefix)))
+(defun-prefix-alt shk-tabbar-next (tabbar-forward-tab) (tabbar-forward-group) (tabbar-mode 1))
+(defun-prefix-alt shk-tabbar-prev (tabbar-backward-tab) (tabbar-backward-group) (tabbar-mode 1))
+(global-set-key[(control tab)] 'shk-tabbar-next)
+(global-set-key[(control tab)] 'shk-tabbar-prev)
+
+;; GUIで直接ファイルを開いた場合フレームを作成しない
+(add-hook 'before-make-frame-hook
+          (lambda()
+            (when(eq tabbar-mode t)
+              (switch-to-buffer (buffer-name))
+              (delete-this-frame))))
+
+;; -----------------------------------------------------
+;; @multi-term.el
+
+;; 端末エミュレータ
+;; http://www.emacswiki.org/emacs/multi-term.el
+(when (require 'multi-term nil t)
+  (setq multi-term-program "/usr/local/bin/zsh"))
+
+;; -----------------------------------------------------
+;; @auto-async-byte-compile.el
+
+;; 自動コンパイル
+;; http://www.emacswiki.org/emacs/auto-async-byte-compile.el
+(when (require 'auto-async-byte-compile nil t)
+  ;; 自動コンパイルを無効にするファイル名の正規表現
+  (setq auto-async-byte-compile-exclude-files-regexp "init.el")
+  (add-hook 'emacs-lisp-mode-hook 'enable-auto-async-byte-compile-mode))
+
+;; -----------------------------------------------------
+;; @uniquify.el
+
+;; 同盟バッファをわかりやすくする
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+(setq uniquify-buffer-name-style 'post-forward-angel-brackets)
+(setq uniquify-ignore-buffers-re "*[^*]+*")
+
+;; ----------------------------------------------------
+;; @auto-complete.el
+
+;; 自動補完機能
+;; https://github.com/m2ym/auto-complete
+(when (require 'auto-complete-config nil t)
+  (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+  (setq ac-ignore-case t)
+  (ac-config-default))
+
+;; ---------------------------------------------------
+;; @eldoc.el/eldoc-extension.el
+
+;; カーソル付近にあるEmacs Lispの関数や変数のヘルプをエコーエリアに表示
+;; http://www.emacswiki.org/emacs/eldoc-extension.el
+(when (require 'eldoc-extension nil t)
+  (add-hook 'emacs-lisp-mode-hook (turn-on-eldoc-mode)
+            (add-hook 'lisp-interaction-mode-hook 'tum-on-eldoc-mode)
+            (add-hook 'ielm-mode-hook 'tum-on-eldoc-mode)
+            (setq eldoc-idle-delay 0.2)
+            (setq eldoc-minor-mode-string ""))
