eac93291d7ab7b3251aeaff1a5d6d2490288b50f
ykazuki <y.kazuki0614n@gmail.com>
2016-04-25 21:39:42 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,19 +1,5 @@
-              ;-*- Mode:Emacs-Lisp;Coding: utf-8 -*-
-;; --------------------------------------------------
-;; @load-path
+				      ;-*- Mode:Emacs-Lisp;Coding: utf-8 -*-
 
-;; load-pathの追加関数
-(defun add-to-load-path (&rest paths)
-  (let (path)
-    (dolist (path paths paths)
-      (let ((default-directory (expand-file-name (concat user-emacs-directory path))))
-  (add-to-list 'load-path default-directory)
-  (if (fboundp 'normal-top-level-add-subdirs-to-load-path)
-      (normal-top-level-add-subdirs-to-load-path))))))
-
-;; load-pathに追加するフォルダ
-;; 2つ以上フォルダを指定する場合の引数 => (add-to-path "elisp" "xxx" "xxx")
-(add-to-load-path "elisp")
 
 ;; -------------------------------------------------
 ;; 文字コード
@@ -298,4 +284,3 @@
      (if (equal nil arg)
          ,on-no-prefix
        ,on-prefix)))
-
