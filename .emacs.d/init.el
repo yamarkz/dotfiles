f195390d0bf0439a8c1d558e8d0f27e00ec7eb26
ykazuki <y.kazuki0614n@gmail.com>
2016-05-24 20:42:51 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -294,6 +294,26 @@
 (cua-mode t)
 (setq cua-enable-cua-kets nil)
 
+;; neotree
+
+(global-set-key [f8] 'neotree-toggle)
+
+;; 隠しファイルをデフォルトで表示
+(setq neo-show-hidden-files t)
+
+;; neotree でファイルを新規作成した後、自動的にファイルを開く
+(setq neo-create-file-auto-open t)
+
+;; delete-other-window で neotree ウィンドウを消さない
+(setq neo-persist-show t)
+
+;; キーバインドをシンプルにする
+(setq neo-keymap-style 'concise)
+
+;; neotree ウィンドウを表示するごとに current file のあるディレクトリを表示する
+(setq neo-smart-open t)
+
+
 ;; ------------------------------------------------------------
 ;; @modeline
 
