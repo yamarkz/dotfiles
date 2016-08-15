8fab97d2c9b22697cc09f3a231ccba5202c57b4f
ykazuki <y.kazuki0614n@gmail.com>
2016-08-15 23:12:35 +0900

    v2 create

---
+++ v2/.emacs.d/.cask/24.5.1/elpa/yard-mode-20160310.850/yard-mode-pkg.el
@@ -0,0 +1,1 @@
+(define-package "yard-mode" "20160310.850" "Minor mode for Ruby YARD comments" 'nil :url "https://github.com/pd/yard-mode.el")
---
+++ v2/.emacs.d/batch-byte-compile.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+## .emacs.d/ 直下で実行
+emacs -batch -f batch-byte-compile inits/*.el
